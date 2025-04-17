65722e0c10cb061ba9bcfea55a7fbdad164acec8
charlotte1227226 <andy20011019@gmail.com>
2025-04-17 21:53:38 +0800

    update number of questions

--- score.c
+++ score.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
+    printf("Welcome to the score calculator!\n");
+    printf("please input the number of questions:\n");
+    int num_questions;
+    scanf("%d", &num_questions);
     while(1){
-        int pre_score[7]={0};
-        int check[7]={0};
+        int pre_score[100]={0};
+        int check[100]={0};
         // for(int i=0;i<7;i++){
         //     printf("pre_score[%d] = %d\n", i, pre_score[i]);
         //     printf("check[%d] = %d\n", i, check[i]);
@@ -11,7 +15,7 @@ int main(){
         int Previous_score = 0;
         int Final_score = 0;
         printf("Yellow sheet score:\n");
-        for(int i=0;i<7;i++){
+        for(int i=0;i<num_questions;i++){
             printf("Enter the score for question %d (Yellow sheet), enter -1 to exit:\n", i+1);
             scanf("%d",&pre_score[i]);
         }
@@ -20,17 +24,17 @@ int main(){
             break;
         }
         printf("White sheet score:\n");
-        for(int i=0;i<7;i++){
+        for(int i=0;i<num_questions;i++){
             printf("Enter the score for question %d (white sheet), enter -1 to exit:\n", i+1);
             scanf("%d",&check[i]);
         }
         if(check[0]==-1){
             break;
         }
-        for(int i=0;i<7;i++){
+        for(int i=0;i<num_questions;i++){
             Previous_score+=pre_score[i];
         }
-        for(int i=0;i<7;i++){
+        for(int i=0;i<num_questions;i++){
             if(pre_score[i]<check[i]){
                 Final_score+=check[i];
             }
